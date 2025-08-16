30940a1a142f8f360e2a6954835f491a708247b9
Nguyen Nam Duong <nguyennamduong205@gmail.com>
2025-08-16 19:27:32 +0700

    Push client struct

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Vite app project</p>
+    </>
+  )
+}
+
+export default App
